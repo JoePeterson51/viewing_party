28a58760fd47257c260ad2138cd72904da27b310
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-13 15:32:29 -0600

    add validations to friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates_confirmation_of :password
 
   has_secure_password
 
-  has_many :friendships
-  has_many :friends, through: :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end

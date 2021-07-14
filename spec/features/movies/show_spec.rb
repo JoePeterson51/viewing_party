987f8a174b2e03e00094f8f63e2b3215c23529f8
JoePeterson51 <>
2021-07-14 16:11:11 -0600

    Test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -21,7 +21,15 @@ RSpec.describe 'movies show' do
         click_on "The Tomorrow War"
         save_and_open_page
         expect(page).to have_content("The Tomorrow War")
-        expect(page).to have_content("Chris Pratt")
+        expect(page).to have_content("Rating: 8.3")
+        expect(page).to have_content("Length: 2 Hours 18 Minutes")
+        expect(page).to have_content("Genere(s): Action, Science Fiction")
+        expect(page).to have_content("The world is stunned when a group of time travelers arrive from the year 2051 to deliver an urgent message: Thirty years in the future, mankind is losing a global war against a deadly alien species. The only hope for survival is for soldiers and civilians from the present to be transported to the future and join the fight. Among those recruited is high school teacher and family man Dan Forester. Determined to save the world for his young daughter, Dan teams up with a brilliant scientist and his estranged father in a desperate quest to rewrite the fate of the planet.")
+        expect(page).to have_content("Character: Dan Forester")
+        expect(page).to have_content("Actor: Chris Pratt")
+        expect(page).to have_content("Username: Kamurai")
+        expect(page).to have_content("Rating: 8.0")
+        expect(page).to have_content("Reviews")
       end
     end
   end

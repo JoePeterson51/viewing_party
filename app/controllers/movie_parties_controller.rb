5ae9c281fa7772e79d22043a57c854854201d0e7
JoePeterson51 <>
2021-07-14 20:51:54 -0600

    Create movie parties

--- app/controllers/movie_parties_controller.rb
+++ app/controllers/movie_parties_controller.rb
@@ -5,7 +5,32 @@ class MoviePartiesController < ApplicationController
   end
 
   def create
-
+    params.merge!(date: (params["time(3i)"] + '/' + params["time(2i)"] + '/' + params["time(1i)"]))
+    params.merge!(start_time: (params["time(4i)"] + ':' + params["time(5i)"]))
+    params.merge!(user_id: current_user.id)
+    params.merge!(movie_api_id: params["movie_id"])
+    params.merge!(movie_title: params["title"])
+    new_movie_party = MovieParty.create(movie_party_params)
+    if new_movie_party.save
+      params["friend"].each do |friend|
+        params.merge!(user_id: friend.to_i)
+        params.merge!(movie_party_id: params["movie_id"])
+        Invite.create(invite_params)
+      end
+      redirect_to user_dashboard_index_path
+    else
+      render :new
+    end
   end
-  # "time(3i)"
+
+private
+
+
+  def movie_party_params
+    params.permit(:movie_api_id, :date, :start_time, :duration, :user_id)
+  end
+
+  def invite_params
+    params.permit(:user_id, :movie_party_id)
+  end
 end
